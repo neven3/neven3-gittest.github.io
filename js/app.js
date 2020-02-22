58c09357f50ff4ecfade93951f6aff8b938984e6
Neven <kosovec_neven@yahoo.com>
2020-02-22 01:59:04 +0100

    Complete project

---
+++ js/app.js
@@ -0,0 +1,35 @@
+const prevButton = document.querySelector('.prev');
+const nextButton = document.querySelector('.next');
+const images = document.querySelectorAll('.gallery-img');
+
+let indexInImages = 0;
+
+function disableButton() {
+    if (indexInImages === 0) {
+        prevButton.setAttribute('disabled', 'true');
+    } else if (indexInImages === images.length - 1) {
+        nextButton.setAttribute('disabled', 'true');
+    } else {
+        prevButton.removeAttribute('disabled');
+        nextButton.removeAttribute('disabled');
+    }
+}
+
+function showImage() {
+    images[indexInImages].classList.toggle('active');
+    if (event.target === prevButton) {
+        indexInImages -= 1;
+    } else if (event.target === nextButton) {
+        indexInImages += 1;
+    }
+
+    images[indexInImages].classList.toggle('active');  
+}
+
+function handleClick(event) {
+    showImage();
+    disableButton();
+}
+
+prevButton.addEventListener('click', handleClick);
+nextButton.addEventListener('click', handleClick);
